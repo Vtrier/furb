9b976a348a9e71824227ab63cced462d3211cf0c
Vtrier <168623374+Vtrier@users.noreply.github.com>
2024-09-23 20:11:01 -0300

    Add files via upload

---
+++ Unidade5/Uni4Ex03.java
@@ -0,0 +1,12 @@
+
+public class Uni4Ex03 {
+
+    public static void main(String[] args) {
+        int i;
+        double soma = 0;
+        for (i = 1; i < 101; i++) {
+            soma += 1.0/i;
+        }
+        System.out.println(soma);
+    }
+}
