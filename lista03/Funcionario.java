f3c5dd323d193aa3b6ee25ebb7f00718f17a0b93
Vtrier <168623374+Vtrier@users.noreply.github.com>
2025-03-21 20:47:41 -0300

    Add files via upload

--- lista03/Funcionario.java
+++ lista03/Funcionario.java
@@ -23,11 +23,25 @@ public class Funcionario {
     }
 
     public void setSalario(double salario) {
+        if (salario < 0) {
+            throw new IllegalArgumentException("ERRO: Salário não pode ser negativo");
+        }
         this.salario = salario;
     }
-    
-    public double calcularIrpf(){
-        
-    }
 
+    public double calcularIrpf(double salario) {
+        double imposto = 0;
+        if (salario <= 1903.98) {
+            imposto = 0;
+        } else if (salario > 1903.98 && salario < 2826.65) {
+            imposto = (2826.65 - 1903.98) * 0.075;
+        } else if (salario > 2826.65 && salario <= 3751.06) {
+            imposto = (2826.65 - 1903.98) * 0.075 + (salario - 2826.65) * 0.15;
+        } else if(salario > 3751.06 && salario <= 4664.68){
+            imposto = (2826.65 - 1903.98) * 0.075 + (3751.06 - 2826.65) * 0.15 + (salario - 3751.06) * 0.225;
+        } else if(salario > 4664.68){
+            imposto = (2826.65 - 1903.98) * 0.075 + (3751.06 - 2826.65) * 0.15 + (4664.68 - 3751.06) * 0.225 + (salario * 0.275);
+        }
+        return imposto;
+    }
 }
