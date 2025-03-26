081020071b511eb479d2457badf49c6e47462c00
Vtrier <168623374+Vtrier@users.noreply.github.com>
2025-03-26 16:09:33 -0300

    Add files via upload

--- Lista06/src/Exercicio/AppLista06GUI.java
+++ Lista06/src/Exercicio/AppLista06GUI.java
@@ -1,6 +1,7 @@
 package Exercicio;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class AppLista06GUI extends javax.swing.JFrame {
 
@@ -18,6 +19,8 @@ public class AppLista06GUI extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         lbSalarioFaixa = new javax.swing.JLabel();
         lbSalario = new javax.swing.JLabel();
+        lbNome = new javax.swing.JLabel();
+        tfNome = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -32,6 +35,10 @@ public class AppLista06GUI extends javax.swing.JFrame {
             }
         });
 
+        lbNome.setText("Nome:");
+
+        tfNome.setPreferredSize(new java.awt.Dimension(100, 20));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -39,33 +46,41 @@ public class AppLista06GUI extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(53, 53, 53)
-                        .addComponent(jButton1))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(36, 36, 36)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lbSalarioFaixa)
+                            .addComponent(lbSalario)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel1)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1)
+                                    .addComponent(lbNome))
                                 .addGap(18, 18, 18)
-                                .addComponent(tfSalario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(lbSalario))))
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(tfNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(tfSalario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(54, 54, 54)
+                        .addComponent(jButton1)))
                 .addContainerGap(70, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(55, 55, 55)
+                .addGap(0, 69, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbNome)
+                    .addComponent(tfNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(tfSalario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(26, 26, 26)
+                .addGap(42, 42, 42)
                 .addComponent(jButton1)
-                .addGap(56, 56, 56)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lbSalarioFaixa)
                 .addGap(18, 18, 18)
                 .addComponent(lbSalario)
-                .addContainerGap(113, Short.MAX_VALUE))
+                .addContainerGap(107, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -83,7 +98,23 @@ public class AppLista06GUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        var funcionario = new Funcionario("Vitor", Integer.parseInt(tfSalario.getText()));
+        String nome = tfNome.getText();
+        if(nome.isEmpty()){
+            JOptionPane.showMessageDialog(this, "ERRO: Campo nome está vazio");
+            return;
+        }
+        
+        String salarioS = tfSalario.getText();
+        
+        
+        if(salarioS.isEmpty()){
+            JOptionPane.showMessageDialog(this, "ERRO: Campo salário está vazio");
+            return;
+        }
+        
+        double salario = Double.parseDouble(salarioS);
+        
+        var funcionario = new Funcionario(nome, salario);
         
         lbSalarioFaixa.setText("O salário está na " + funcionario.identificarFaixaIrpf()+" Faixa.");
         lbSalario.setText(String.format("O imposto a pagar é de R$ %.2f", funcionario.calcularIrpf()));
@@ -104,8 +135,10 @@ public class AppLista06GUI extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lbNome;
     private javax.swing.JLabel lbSalario;
     private javax.swing.JLabel lbSalarioFaixa;
+    private javax.swing.JTextField tfNome;
     private javax.swing.JTextField tfSalario;
     // End of variables declaration//GEN-END:variables
 }
